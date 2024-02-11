e2b272b8cc65a49d276007f70de94afd8caccaa5
Mohit Jaiswal <mohitjaiswal2883@gmail.com>
2024-02-11 11:59:00 +0530

    Student login and signup page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import './App.css'
 import Home from './pages/Home'
 import StudentLogin from './pages/StudentLogin';
+import StudentSigunUp from './pages/StudentSignUp';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/studentlogin" element={<StudentLogin />}></Route>
+        <Route path="/studentsignup" element={<StudentSigunUp />}></Route>
       </Routes>
       {/* <Footer /> */}
     </>
--- src/pages/StudentSignUp.jsx
+++ src/pages/StudentSignUp.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import StudentLoginImg from '../assets/StudentLoginImg.png';
+import StudentSignUpImg from '../assets/StudentSignUpImg.png';
 
-export default function StudentLogin() {
+export default function StudentSigunUp() {
   return (
     <>
       <section id="home" className="flex items-center justify-center w-full h-screen">
         <div className="container mx-32 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
           <div className='grid place-items-center'>
             <div>
-              <h1 className='font-medium'>Welcome Back</h1>
+              <h1 className='font-medium text-5xl'>Join the millions learning to code with StudyNotion for free</h1>
+              <br />
               <p>Build skills for today, tomorrow, and beyond.<span className='font-pacifico'> Education 
               <br />
               to future-proof your career.</span></p>
@@ -20,7 +21,7 @@ export default function StudentLogin() {
 
           <div className='flex justify-center items-center'>
             <div>
-              <img src={StudentLoginImg} alt="Student Login"  className="w-auto h-auto" />
+              <img src={StudentSignUpImg} alt="Student Signup"  className="w-auto h-auto" />
             </div>
           </div>
         </div>
