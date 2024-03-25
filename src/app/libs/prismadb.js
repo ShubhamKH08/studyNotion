6ccf124f02a96515749778633b2a745d0f40422b
ShubhamKH08 <hagawaneshubham908@gmail.com>
2024-03-25 09:39:05 +0530

    Relative Path Corrected inside route.ts

--- src/app/libs/prismadb.js
+++ src/app/libs/prismadb.js
@@ -11,18 +11,22 @@
 // const client = globalObj.client;
 
 // export default client;
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
+
+// let prisma;
+// index.js
+import { PrismaClient } from '@prisma/client';
 
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
+    prisma = new PrismaClient();
 } else {
     if (!global.cachedPrisma) {
-        global.cachedPrisma = new PrismaClient()
+        global.cachedPrisma = new PrismaClient();
     }
-    prisma = global.cachedPrisma
+    prisma = global.cachedPrisma;
 }
 
 export const db = prisma;
